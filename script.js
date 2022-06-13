99ea30eec9f617aeca1c6009990dc8a1cb2197f9
Tomas Pocius <pocius.tomastp@gmail.com>
2022-06-13 22:08:06 +0300

    added painting with hover

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-let container = document.getElementById("container");
+let container = document.querySelector("#container");
+container.style.gridTemplateColumns = "repeat(16, auto)";
 
 for (i = 0; i < 16*16; i++) {       
 let square = container.appendChild(document.createElement("div"));
+let randomColor = Math.floor(Math.random()*16777215).toString(16);
 square.className = "square";
 square.style.border = "thin solid black"
+square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = `#${randomColor}`;
+})
 }
 
-container.style.gridTemplateColumns = "repeat(16, auto)";
 
-let randomColor = Math.floor(Math.random()*16777215).toString(16);
-console.log(typeof(container))
-container.addEventlistener("mouseover", () => {
-    backgroundColor = "blue"
-})
+
+
+
+
+
